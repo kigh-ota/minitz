de317c24b2700bbf7d3ed02a527f8f182ace5d0e
Kaiichiro Ota <kigh.ota@gmail.com>
2018-10-24 21:32:55 +0900

    refactor: KintoneApi class

--- main.js
+++ main.js
@@ -1,71 +1,89 @@
-let isInPeople = false;
-let popup = null;
+class KintoneApi {
+  static fetchComments(dayCount) {
+    return kintone.api('/k/api/people/user/post/list', 'POST', {
+      threadId: kintone.getLoginUser().id,
+      size: 100,
+    }).then(async resp => {
+      const today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+      const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+      // data[n]: n日前
+      let data = [...Array(dayCount).keys()].map(i => {
+        const date = new Date(today.getFullYear(), today.getMonth(), today.getDate() - i);
+        return {
+          date: `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`,
+          commentCount: 0,
+          commentedCount: 0,
+          likeCount: 0,
+          likedCount: 0,
+        }
+      });
+      const posts = resp.result.items.filter(item => {
+        return new Date(today.getFullYear(), today.getMonth(), today.getDate() - dayCount + 1) <= new Date(item.commentedAt);
+      });
+      let comments = posts.slice();
+      for (let post of posts) {
+        if (post.comments.length < post.commentCount) {
+          await kintone.api('/k/api/people/user/comment/list', 'POST', {
+            postId: post.id,
+            size: 200,
+          }).then(resp => {
+            comments.push(...resp.result.items);
+          });
+        } else {
+          comments.push(...post.comments);
+        }
+      }
+      comments.forEach(comment => {
+        const date = new Date(comment.createdAt);
+        if (new Date(today.getFullYear(), today.getMonth(), today.getDate() - dayCount + 1) <= date) {
+          const i = Math.floor((tomorrow - date) / 1000 / 60 / 60 / 24);
+          if (comment.creator.id === kintone.getLoginUser().id) {
+            data[i].commentCount++;
+            data[i].likedCount += comment.likeCount;
+          } else {
+            data[i].commentedCount++;
+          }
+          if (comment.liked) {
+            data[i].likeCount++;
+          }
+        }
+      });
+      return data;
+    });
+  }
 
-const onHashChange = () => {
-  const code = kintone.getLoginUser().code;
-  if (!isInPeople && document.location.hash.startsWith(`#/people/user/${code}`)) {
-    console.log('enter my people page.');
-    popup.show();
-    isInPeople = true;
-  } else if (isInPeople && !document.location.hash.startsWith(`#/people/user/${code}`)) {
-    console.log('leave my people page.')
-    popup.hide();
-    isInPeople = false;
+  static uploadBlob(blob) {
+    const formData = new FormData();
+    formData.append('__REQUEST_TOKEN__', kintone.getRequestToken());
+    formData.append('file', blob, 'hoge.png');
+    
+    const url = kintone.api.url('/k/v1/file');
+    
+    return fetch(url, {
+      method: 'POST',
+      headers: { 'X-Requested-With': 'XMLHttpRequest' },
+      body: formData,
+    }).then(resp => {
+      return resp.json();
+    }).then(json => {
+      console.log(json);
+      return json['fileKey'];
+    }).catch(console.error);
+  }
+
+  static postToPeople(fileKey) {
+    const threadId = kintone.getLoginUser().id;
+    kintone.api('/k/api/people/user/post/add', 'POST',
+      {
+        threadId,
+        body: `<div><img class="cybozu-tmp-file" data-original="/k/api/blob/download.do?fileKey=${fileKey}" width="250" src="/k/api/blob/download.do?fileKey=${fileKey}&w=250" data-file="${fileKey}" data-width="250"></div>`,
+        mentions: [],
+        groupMentions:[],
+        orgMentions: [],
+      }, console.log, console.error
+    );
   }
 };
-
-const fetchComments = (dayCount) => {
-  return kintone.api('/k/api/people/user/post/list', 'POST', {
-    threadId: kintone.getLoginUser().id,
-    size: 100,
-  }).then(async resp => {
-    const today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
-    const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
-    // data[n]: n日前
-    let data = [...Array(dayCount).keys()].map(i => {
-      const date = new Date(today.getFullYear(), today.getMonth(), today.getDate() - i);
-      return {
-        date: `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`,
-        commentCount: 0,
-        commentedCount: 0,
-        likeCount: 0,
-        likedCount: 0,
-      }
-    });
-    const posts = resp.result.items.filter(item => {
-      return new Date(today.getFullYear(), today.getMonth(), today.getDate() - dayCount + 1) <= new Date(item.commentedAt);
-    });
-    let comments = posts.slice();
-    for (post of posts) {
-      if (post.comments.length < post.commentCount) {
-        await kintone.api('/k/api/people/user/comment/list', 'POST', {
-          postId: post.id,
-          size: 200,
-        }).then(resp => {
-          comments.push(...resp.result.items);
-        });
-      } else {
-        comments.push(...post.comments);
-      }
-    }
-    comments.forEach(comment => {
-      const date = new Date(comment.createdAt);
-      if (new Date(today.getFullYear(), today.getMonth(), today.getDate() - dayCount + 1) <= date) {
-        const i = Math.floor((tomorrow - date) / 1000 / 60 / 60 / 24);
-        if (comment.creator.id === kintone.getLoginUser().id) {
-          data[i].commentCount++;
-          data[i].likedCount += comment.likeCount;
-        } else {
-          data[i].commentedCount++;
-        }
-        if (comment.liked) {
-          data[i].likeCount++;
-        }
-      }
-    });
-    return data;
-  });
-}
 
 class Component {
   render(parentEl) {
@@ -90,7 +108,7 @@ class Popup extends Component {
     
     const chartWrapper = document.createElement('DIV');
     this.chart_ = null;
-    fetchComments(7).then(data => {
+    KintoneApi.fetchComments(7).then(data => {
       this.chart_ = new MyChart(data.reverse());
       this.chart_.render(chartWrapper);
     });
@@ -100,8 +118,8 @@ class Popup extends Component {
     button.addEventListener('click', async (event) => {
       const blob = await this.chart_.getImageAsBlob();
       console.log(blob);
-      const fileKey = await uploadBlob(blob);
-      postToPeople(fileKey);
+      const fileKey = await KintoneApi.uploadBlob(blob);
+      KintoneApi.postToPeople(fileKey);
     });
     
     this.el_.appendChild(button);
@@ -116,37 +134,6 @@ class Popup extends Component {
     this.el_.classList.add(Popup.HIDDEN_CSS_CLASS);
   }
 }
-
-const uploadBlob = (blob) => {
-  const formData = new FormData();
-  formData.append('__REQUEST_TOKEN__', kintone.getRequestToken());
-  formData.append('file', blob, 'hoge.png');
-  
-  const url = kintone.api.url('/k/v1/file');
-  
-  return fetch(url, {
-    method: 'POST',
-    headers: { 'X-Requested-With': 'XMLHttpRequest' },
-    body: formData,
-  }).then(resp => {
-    return resp.json();
-  }).then(json => {
-    console.log(json);
-    return json['fileKey'];
-  }).catch(console.error);
-};
-
-const postToPeople = (fileKey) => {
-  threadId = kintone.getLoginUser().id;
-  kintone.api('/k/api/people/user/post/add', 'POST',
-    {
-      threadId,
-      body: `<div><img class="cybozu-tmp-file" data-original="/k/api/blob/download.do?fileKey=${fileKey}" width="250" src="/k/api/blob/download.do?fileKey=${fileKey}&w=250" data-file="${fileKey}" data-width="250"></div>`,
-      mentions: [],
-      groupMentions:[],
-      orgMentions: [],
-    }, console.log, console.error);
-};
 
 class MyChart extends Component {
   constructor(data) {
@@ -204,9 +191,26 @@ class MyChart extends Component {
   }
 }
 
+let isInPeople = false;
+let popup = null;
+
+const onHashChange = () => {
+  const code = kintone.getLoginUser().code;
+  if (!isInPeople && document.location.hash.startsWith(`#/people/user/${code}`)) {
+    console.log('enter my people page.');
+    popup.show();
+    isInPeople = true;
+  } else if (isInPeople && !document.location.hash.startsWith(`#/people/user/${code}`)) {
+    console.log('leave my people page.')
+    popup.hide();
+    isInPeople = false;
+  }
+};
+
 if ('onhashchange' in window) {
   popup = new Popup();
   popup.render(document.body);
   window.addEventListener('hashchange', onHashChange);
   onHashChange();
 }
+
