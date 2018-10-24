95e9d4745d7bb103cb734f38790293e4f6463c60
y-okady <y.okady.221@gmail.com>
2018-10-24 11:09:52 +0900

    ピープルを開いたらポップアップを表示する

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 console.log('Hello minitz!');
+
+const POPUP_CSS_CLASS = 'minitz-popup';
+const POPUP_HIDDEN_CSS_CLASS = 'hidden';
+
+let isInPeople = false;
+const onHashChange = () => {
+  // TODO: 自分のピープルだけにする
+  if (!isInPeople && document.location.hash.startsWith('#/people/user/')) {
+    console.log('enter the people page.');
+    showPopup();
+    isInPeople = true;
+  } else if (isInPeople && !document.location.hash.startsWith('#/people/user/')) {
+    console.log('leave the people page.')
+    hidePopup();
+    isInPeople = false;
+  }
+};
+
+const createPopup = () => {
+  let popup = document.createElement('DIV');
+  popup.classList.add(POPUP_CSS_CLASS);
+  popup.classList.add(POPUP_HIDDEN_CSS_CLASS);
+  popup.textContent = 'Hello minitz!';
+  document.body.appendChild(popup);
+}
+
+const showPopup = () => {
+  document.getElementsByClassName(POPUP_CSS_CLASS)[0].classList.remove(POPUP_HIDDEN_CSS_CLASS);
+};
+
+const hidePopup = () => {
+  document.getElementsByClassName(POPUP_CSS_CLASS)[0].classList.add(POPUP_HIDDEN_CSS_CLASS);
+}
+
+if ('onhashchange' in window) {
+  createPopup();
+  window.addEventListener('hashchange', onHashChange);
+  onHashChange();
+}
