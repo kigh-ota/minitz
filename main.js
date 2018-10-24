ae34af42989069a0461e15522b9d93718b020118
kaiichiro-ota <kaiichiro-ota@cybozu.co.jp>
2018-10-24 13:35:46 +0900

    draw dummy chart

--- main.js
+++ main.js
@@ -16,11 +16,61 @@ const onHashChange = () => {
 };
 
 const createPopup = () => {
-  let popup = document.createElement('DIV');
+  const popup = document.createElement('DIV');
   popup.classList.add(POPUP_CSS_CLASS);
   popup.classList.add(POPUP_HIDDEN_CSS_CLASS);
   popup.textContent = 'Hello minitz!';
+
+  appendChart(popup);
+
   document.body.appendChild(popup);
+}
+
+const appendChart = (parentEl) => {
+  const canvasEl = document.createElement('CANVAS');
+  const chart = new Chart(canvasEl, {
+    type: 'bar',
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true,
+          }
+        }]
+      },
+      elements: {
+        line: {
+          tension: 0,
+        }
+      }
+    },
+    type: 'line',
+  });
+  parentEl.appendChild(canvasEl);
 }
 
 const showPopup = () => {
