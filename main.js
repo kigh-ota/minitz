38233ad621aa1ea5250b1559c21169881ab267c9
kaiichiro-ota <kaiichiro-ota@cybozu.co.jp>
2018-10-24 13:45:26 +0900

    refactor

--- main.js
+++ main.js
@@ -21,56 +21,50 @@ const createPopup = () => {
   popup.classList.add(POPUP_HIDDEN_CSS_CLASS);
   popup.textContent = 'Hello minitz!';
 
-  appendChart(popup);
+  const myChartWrapper = document.createElement('DIV');
+  popup.appendChild(myChartWrapper);
+  const myChart = new MyChart([12, 19, 3, 5, 2, 3]);
+  myChart.render(myChartWrapper);
 
   document.body.appendChild(popup);
 }
 
-const appendChart = (parentEl) => {
-  const canvasEl = document.createElement('CANVAS');
-  const chart = new Chart(canvasEl, {
-    type: 'bar',
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero:true,
-          }
+class MyChart {
+  constructor(data) {
+    this.canvasEl_ = document.createElement('CANVAS');
+    this.chart_ = new Chart(this.canvasEl_, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: data,
+          backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+          borderColor: ['rgba(255,99,132,1)'],
+          borderWidth: 1
         }]
       },
-      elements: {
-        line: {
-          tension: 0,
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true,
+            }
+          }]
+        },
+        elements: {
+          line: {
+            tension: 0,
+          }
         }
-      }
-    },
-    type: 'line',
-  });
-  parentEl.appendChild(canvasEl);
+      },
+      type: 'line',
+    });
+  }
+
+  render(parentEl) {
+    parentEl.appendChild(this.canvasEl_);
+  }
 }
 
 const showPopup = () => {
